0511e3b8c7caba98bcb70ff38d90330f5288ed17
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2021-10-20 01:14:09 -0400

    successful api call to NASA API

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// GET events page
+router.get('/events', (req, res, next) => {
+  request('https://eonet.sci.gsfc.nasa.gov/api/v3/events?limit=5&days=20&source=InciWeb&status=open', function (error, response, body) {
+    if(error) {
+      console.error('error:', error); // Print the error if one occurred
+    }
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    console.log('body:', body); // Print the response for the API.
+    res.send(body);
+  });
+});
+
+
 
 module.exports = router;
