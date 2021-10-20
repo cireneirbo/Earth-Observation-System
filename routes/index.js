d32b46d75df9cdcb08839b0afc41933fb7e521aa
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2021-10-20 01:14:53 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const request = require('request');
+const request = require('request'); // library used to make API calls
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
