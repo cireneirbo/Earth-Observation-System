d86c8b6ba0d81dd0e3a55f430e73eeb4feeb7ed8
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2021-11-04 22:59:56 -0400

    Add Axios and return data

--- api-returns/eonet.mjs
+++ api-returns/eonet.mjs
@@ -1,9 +1,9 @@
-import fetch from "node-fetch";
+// import fetch from "node-fetch";
 
-function apiCall() {
-    const apiResponse = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v3/events?limit=5&days=200&source=InciWeb&status=open')
-    const data = await apiResponse.json();
-    console.log(data);
-}
+// function apiCall() {
+//     const apiResponse = await fetch('https://eonet.sci.gsfc.nasa.gov/api/v3/events?limit=5&days=200&source=InciWeb&status=open')
+//     const data = await apiResponse.json();
+//     console.log(data);
+// }
 
-module.exports = apiCall;
+// module.exports = apiCall;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 //const apiCall = require("../api-returns/eonet.mjs");
+const axios = require("axios").default; // default allows for typescript intellisense, if necessary
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,8 +10,30 @@ router.get('/', function(req, res, next) {
 
 /* GET events page. */
 router.get('/events', function(req, res, next) {
-  res.render('events', { title: 'Events' });
-  //res.send(apiCall());
+  //let response = getEvents();
+  //console.log(getEvents());
+  //res.render('events', { data: response });
+  res.send(getEvents());
+  //getEvents();
+  
+  
 });
+
+// Make a request for a user with a given URL
+function getEvents() {
+  axios.get('https://eonet.sci.gsfc.nasa.gov/api/v3/events?limit=5&days=400&source=InciWeb&status=open')
+    .then(function (response) {
+      // handle success
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+      console.log("End");
+    });
+}
 
 module.exports = router;
