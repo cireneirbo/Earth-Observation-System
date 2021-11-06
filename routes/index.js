823f7c1ff2dc48cbea7271dba1af37ee2ccfd59e
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2021-11-06 18:57:23 -0400

    Add module for retrieving EONET API requests

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
+const events = require("../api-returns/events");
 //const apiCall = require("../api-returns/eonet.mjs");
-const axios = require("axios").default; // default allows for typescript intellisense, if necessary
+const axios = require("axios").default; // .default allows for typescript intellisense, if necessary
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,30 +11,10 @@ router.get('/', function(req, res, next) {
 
 /* GET events page. */
 router.get('/events', function(req, res, next) {
-  //let response = getEvents();
-  //console.log(getEvents());
+
   //res.render('events', { data: response });
-  res.send(getEvents());
-  //getEvents();
-  
+  res.json(events.getEvents());
   
 });
-
-// Make a request for a user with a given URL
-function getEvents() {
-  axios.get('https://eonet.sci.gsfc.nasa.gov/api/v3/events?limit=5&days=400&source=InciWeb&status=open')
-    .then(function (response) {
-      // handle success
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-    })
-    .then(function () {
-      // always executed
-      console.log("End");
-    });
-}
 
 module.exports = router;
