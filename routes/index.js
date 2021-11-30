75d774b188a659891bdf68142e4766f6efeb3954
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2021-11-30 17:52:39 -0500

    Change some vars to consts

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const events = require("../controllers/events");
 const axios = require("axios").default; // .default allows for typescript intellisense, if necessary
 
