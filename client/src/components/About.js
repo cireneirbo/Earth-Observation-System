f5103df8454aec2cb7606c3ff01754d4c1c0aed9
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2022-02-11 15:22:12 -0500

    Add to About

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -5,10 +5,18 @@ function About() {
   return (
     <main>
       <fieldset className="page-field">
-        <h1>About</h1>
+      <h1>About</h1>
+
+      <h3>Earth Observatory Natural Event Tracker API</h3>
         <p>
-          About
+          Disaster events are returned from NASA's <a href='https://eonet.gsfc.nasa.gov/docs/v3' target='_blank'>EONET</a> API and parsed for new events to be handled.
         </p>
+
+      <h3>Twitter API</h3>
+        <p>
+        Data from the EONET API is tracked internally, and new events are posted to the <a href='https://twitter.com/kalefice' target='_blank'>developer's</a> Twitter account. No user data is sent via the Twitter API.
+        </p>
+
       </fieldset>
         
     </main>
