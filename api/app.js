c73ed4ff766b94025f95455720b7f535d8b301fb
Eric O'Brien <36604687+cireneirbo@users.noreply.github.com>
2022-06-07 21:05:47 -0400

    Prepare to switch to nodemailer for emails

--- api/app.js
+++ api/app.js
@@ -5,6 +5,8 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
 require('dotenv').config();
 
 const indexRouter = require('./routes/index');
@@ -20,6 +22,8 @@ app.set('view engine', 'pug');
 
 app.use(helmet());
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
